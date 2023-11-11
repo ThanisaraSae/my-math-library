0a237407e5b7c17fba12417608e6d7e63ddd36d6
Japan <Thanisara.sae@dome.tu.ac.th>
2023-11-11 11:39:52 +0700

    Array.forEach

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+// function sum(a, b) {
+//     return a + b
+//   }
+  
+//   module.exports = {
+//     sum,
+//   }
+
 function sum(a, b) {
+  if (Array.isArray(a)) {
+    let sum = 0
+    a.forEach((i) => (sum += i))
+    return sum
+  } else if (typeof a === 'number' && typeof b === 'number') {
     return a + b
   }
-  
-  module.exports = {
-    sum,
-  }
-  
+}
+
+   module.exports = {
+     sum,
+   } 
